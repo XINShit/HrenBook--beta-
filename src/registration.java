aa2fb1d62212fbb39096a9a4b609636e2f239e4f
betkapolymorphic <a_albert@ukr.net>
2014-07-18 12:09:05 +0300

    Wordking registration on neo4j

--- src/registration.java
+++ src/registration.java
@@ -1,5 +1,9 @@
 
 
+import hrenbook.engine.MainEngine;
+import org.neo4j.graphdb.DynamicLabel;
+import org.neo4j.graphdb.Label;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -19,7 +23,10 @@ public class registration extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         if(req.getParameter("action") != null) {
-
+            MainEngine mainEngine = new MainEngine();
+            mainEngine.Register(req.getParameter("login"),req.getParameter("password"),
+                    req.getParameter("name"),req.getParameter("lastname"), Integer.valueOf(req.getParameter("age")));
+            Label q = DynamicLabel.label("q");
         }
         req.getRequestDispatcher("/jsp/registration.jsp").forward(req, resp);
     }
